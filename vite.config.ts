d61c7a629e585efbc8fb5ff466974714cf98cb06
Thomas Tuan <hinchuon@gmail.com>
2025-10-30 19:38:48 +0700

    update proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/receiver': 'http://127.0.0.1:5000',
-      '/api': 'http://127.0.0.1:5000'
-    }
-  } 
+export default defineConfig(({ mode }) => {
+  // Đọc biến môi trường từ file .env, .env.development, .env.production, v.v.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/receiver': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  }
 })
